45360ac694ea59a20d046e3dcfee11eda3e5bdf5
TiwariAyush2002 <ayushtiwari20082002@gmail.com>
2022-12-14 15:38:13 +0530

    Passenger Counter App

--- CounterApp/index.js
+++ CounterApp/index.js
@@ -1,4 +1,17 @@
-// document.getElementById("count-el").innerText = 10
-function increment(){
-    console.log("button was clicked")
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+console.log(saveEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+function save(){
+    let countStr=count + "-"
+    saveEl.innerText+=countStr
+    countEl.textContent=0
+    count=0
+    console.log(count)
 }
